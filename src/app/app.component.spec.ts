9598602920be8086770cc9e9a8471a3316c6e6eb
Anderson E. Peñaloza C <designeromega@gmail.com>
2022-01-15 18:36:29 +0100

    add test lifecicles events

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,11 +3,27 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let appComponent: AppComponent;
+
+  beforeAll(() => {
+    console.log('beforeAll execute');
+  });
+
+  afterAll(() => {
+    console.log('afterAll execute');
+  });
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent]
     }).compileComponents();
+    console.log('beforeEach execute');
+    appComponent = new AppComponent();
+  });
+
+  afterEach(() => {
+    console.log('afterEach execute');
   });
 
   it('should create the app', () => {
@@ -17,24 +33,20 @@ describe('AppComponent', () => {
   });
   // Test equal in a var
   it('myVar should be equal to Hola Mundo', () => {
-    const appComponent = new AppComponent();
     const valor = appComponent.myVar;
     expect(valor).toEqual('Hola Mundo');
   });
   // Test if a var contains a value
   it('nombre should contain Jhon', () => {
-    const appComponent = new AppComponent();
     const valor = appComponent.nombre;
     expect(valor).toContain('Jhon');
   });
   // test Booleans
   it('it should be Par', () => {
-    const appComponent = new AppComponent();
     const valor = appComponent.par(44);
     expect(valor).toBeTruthy();
   });
   it('it should be Inpar', () => {
-    const appComponent = new AppComponent();
     const valor = appComponent.par(41);
     expect(valor).toBeFalsy();
   });
